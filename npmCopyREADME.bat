96e909665770d666deadc5c21564c0ac0e29bc24
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2022-05-31 16:08:16 +0300

    Added EntityFrameworkCore.3.1.25 package

--- npmCopyREADME.bat
+++ npmCopyREADME.bat
@@ -1,1 +1,3 @@
-xcopy .\README.md .\Assets\_PackageRoot\README.md /y
+xcopy .\README.md .\_EntityFrameworkCore.3.1.25\_PackageRoot\README.md /y
+xcopy .\README.md .\_EntityFrameworkCore.Proxies.3.1.25\_PackageRoot\README.md /y
+xcopy .\README.md .\_EntityFrameworkCore.SQLite.3.1.25\_PackageRoot\README.md /y
--- npmPublish.bat
+++ npmPublish.bat
@@ -1,4 +1,19 @@
-xcopy .\README.md .\Assets\_PackageRoot\README.md /y
-cd Assets\_PackageRoot
+xcopy .\README.md .\Assets\_EntityFrameworkCore.3.1.25\README.md /y
+cd Assets\_EntityFrameworkCore.3.1.25
 npm publish
+cd ..
+cd ..
+
+xcopy .\README.md .\Assets\_EntityFrameworkCore.Proxies.3.1.25\README.md /y
+cd Assets\_EntityFrameworkCore.Proxies.3.1.25
+npm publish
+cd ..
+cd ..
+
+xcopy .\README.md .\Assets\_EntityFrameworkCore.SQLite.3.1.25\README.md /y
+cd Assets\_EntityFrameworkCore.SQLite.3.1.25
+npm publish
+cd ..
+cd ..
+
 pause
