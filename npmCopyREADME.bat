9a2b42472e137613357d373c7b528bb9afe088d4
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2022-06-01 03:53:42 +0300

    Package information update

--- npmCopyREADME.bat
+++
@@ -1,3 +0,0 @@
-xcopy .\README.md .\_EntityFrameworkCore.3.1.25\_PackageRoot\README.md /y
-xcopy .\README.md .\_EntityFrameworkCore.Proxies.3.1.25\_PackageRoot\README.md /y
-xcopy .\README.md .\_EntityFrameworkCore.SQLite.3.1.25\_PackageRoot\README.md /y
--- npmPublish.bat
+++ npmPublish.bat
@@ -1,16 +1,13 @@
-xcopy .\README.md .\Assets\_EntityFrameworkCore.3.1.25\README.md /y
 cd Assets\_EntityFrameworkCore.3.1.25
 npm publish
 cd ..
 cd ..
 
-xcopy .\README.md .\Assets\_EntityFrameworkCore.Proxies.3.1.25\README.md /y
 cd Assets\_EntityFrameworkCore.Proxies.3.1.25
 npm publish
 cd ..
 cd ..
 
-xcopy .\README.md .\Assets\_EntityFrameworkCore.SQLite.3.1.25\README.md /y
 cd Assets\_EntityFrameworkCore.SQLite.3.1.25
 npm publish
 cd ..
